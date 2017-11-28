6de45a3ce1d80c196902676ae587e33b0e216a10
EdGEgar44 <34071975+EdGEgar44@users.noreply.github.com>
2017-11-28 10:46:02 -0800

    Added previus notes
    11.27.17

--- Notes.py
+++ Notes.py
@@ -1,1 +1,30 @@
-# This is working
+print("Hello World")
+
+# Edgar Renteria
+# (This is python 2.7)
+
+print(3 + 5)
+print(5 - 3)
+print(3 * 5)
+print(6 / 2)
+print(3 ** 2)
+
+# This makes a new line. In phyton 3.6, it looks like: print()
+print("see if you can figure this out")
+print(20 % 21)
+
+car_name = "Wiebe Mobile"
+car_type = "Tesla"
+car_cylinders = 8
+car_mpg = 9000.1
+
+# Inline printing
+print("I have a car called the %s. It's a %s." % (car_name, car_type))
+
+
+# asking for input
+name = input("What is your name? ")  # In phyton 3, it's just called input()
+print("Hello %s." % name)
+
+age = input("How old are you? ")
+print("%s!? Wow you are older than Jesus." % age)

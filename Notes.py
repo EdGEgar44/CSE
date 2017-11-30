6382eefeecdceb0b83926ead774c88a4f0ccfa74
EdGEgar44 <34071975+EdGEgar44@users.noreply.github.com>
2017-11-30 10:47:14 -0800

    Added Loops
    11.30.17

--- Notes.py
+++ Notes.py
@@ -1,33 +1,36 @@
-# print("Hello World")
-#
-# # Edgar Renteria
-# # (This is python 2.7)
-#
-# print(3 + 5)
-# print(5 - 3)
-# print(3 * 5)
-# print(6 / 2)
-# print(3 ** 2)
-#
-# # This makes a new line. In phyton 3.6, it looks like: print()
-# print("see if you can figure this out")
-# print(20 % 21)
-#
-# car_name = "Wiebe Mobile"
-# car_type = "Tesla"
-# car_cylinders = 8
-# car_mpg = 9000.1
-#
-# # Inline printing
-# print("I have a car called the %s. It's a %s." % (car_name, car_type))
-#
-#
-# # asking for input
-# name = input("What is your name? ")  # In phyton 3, it's just called input()
-# print("Hello %s." % name)
-#
-# age = input("How old are you? ")
-# print("%s!? Wow you are older than Jesus." % age)
+import random  # This should be on line 1
+
+
+"""print("Hello World")
+
+# Edgar Renteria
+# (This is python 2.7)
+
+print(3 + 5)
+print(5 - 3)
+print(3 * 5)
+print(6 / 2)
+print(3 ** 2)
+
+# This makes a new line. In phyton 3.6, it looks like: print()
+print("see if you can figure this out")
+print(20 % 21)
+
+car_name = "Wiebe Mobile"
+car_type = "Tesla"
+car_cylinders = 8
+car_mpg = 9000.1
+
+# Inline printing
+print("I have a car called the %s. It's a %s." % (car_name, car_type))
+
+
+# asking for input
+name = input("What is your name? ")  # In phyton 3, it's just called input()
+print("Hello %s." % name)
+
+age = input("How old are you? ")
+print("%s!? Wow you are older than Jesus." % age)
 
 # Functions
 
@@ -46,7 +49,7 @@ def say_hi(name):  # name is a parameter
     print("Enjoy your day.")
 
 
-say_hi("john")
+say_hi("John")
 
 
 def print_age(name, age):
@@ -59,7 +62,7 @@ print_age("John", 15)
 
 
 def f(x):
-    return x**3 + 4 * x**2 + 7 * x - 4
+     return x**3 + 4 * x**2 + 7 * x - 4
 
 
 print(f(3))
@@ -70,16 +73,44 @@ print(f(5))
 
 
 def grade_calc(percentage):
-    if percentage >= 90:
-        return "A"
-    elif percentage >= 80:
-        return "B"
-    elif percentage >= 70:
-        return "C"
-    elif percentage >= 60:
-        return "D"
-    else:
-        return "F"
+     if percentage >= 90:
+         return "A"
+     if percentage >= 80:
+         return "B"
+     if percentage >= 70:
+         return "C"
+     if percentage >= 60:
+         return "D"
+     if percentage <= 50:
+         return "F"
 
 
 print(grade_calc(8))
+
+
+def happy_bday(name):
+    print("Happy Birthday to you.")
+    print("Happy Birthday to you.")
+    print("Happy Birthday dear " + name)
+    print("happy Birthday to you.")
+
+
+happy_bday("John")
+
+# Loops
+
+for num in range(1000000):
+    print(num + 1)
+
+
+# DO NOT RUN!!!
+a = 1
+while a < 10:
+    print(a)
+    a += 1 """
+
+
+# random nubers
+
+
+print(random.randint(0, 1001))

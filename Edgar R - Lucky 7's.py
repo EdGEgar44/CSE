cd9ad40afdb5e958298ee99fe6f63219730b53ee
EdGEgar44 <34071975+EdGEgar44@users.noreply.github.com>
2017-12-11 10:49:29 -0800

    12.11.17
    Finished Lucky 7's

--- Edgar R - Lucky 7's.py
+++ Edgar R - Lucky 7's.py
@@ -4,11 +4,30 @@ rounds = 0
 
 money = 15
 
-r = random.randint(1, 6)
-r2 = random.randint(1, 6)
-
-
-
 while money > 0:
-    if (r + r2) == 7:
-        print("you gain ")
+    print("Type yes to continues. Type no to stop")
+    bet = input("Bet $1 to get four more dollars? ")
+
+    if bet == "yes":
+        r = random.randint(1, 6)
+
+        r2 = random.randint(1, 6)
+
+        print("You made a bet of one dollar")
+
+        if (r + r2) == 7:
+            money += 5
+            print("You won your bet.")
+            print("you gain your money and a bonus")
+            print("You know have $ %s " % money)
+            rounds += 1
+
+        if (r + r2) != 7:
+            money -= 1
+            print("You lost your bet")
+            print("You have  $ %s left." % money)
+            rounds += 1
+
+print("You lost all of your money")
+
+print("You did %s rounds." % rounds)

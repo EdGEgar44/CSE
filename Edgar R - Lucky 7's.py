08a33a07495a57f01c4cb694e435c91035a5e4b8
EdGEgar44 <34071975+EdGEgar44@users.noreply.github.com>
2017-12-12 12:41:35 -0800

    12.12.17
    Infinite solutions

--- Edgar R - Lucky 7's.py
+++ Edgar R - Lucky 7's.py
@@ -5,28 +5,25 @@ rounds = 0
 money = 15
 
 while money > 0:
-    print("Type yes to continues. Type no to stop")
-    bet = input("Bet $1 to get four more dollars? ")
 
-    if bet == "yes":
-        r = random.randint(1, 6)
+    r = random.randint(1, 6)
 
-        r2 = random.randint(1, 6)
+    r2 = random.randint(1, 6)
 
-        print("You made a bet of one dollar")
+    print("You made a bet of one dollar")
 
-        if (r + r2) == 7:
-            money += 5
-            print("You won your bet.")
-            print("you gain your money and a bonus")
-            print("You know have $ %s " % money)
-            rounds += 1
+    if (r + r2) == 7:
+        money += 5
+        print("You won your bet.")
+        print("you gain your money and a bonus")
+        print("You know have $ %s " % money)
+        rounds += 1
 
-        if (r + r2) != 7:
-            money -= 1
-            print("You lost your bet")
-            print("You have  $ %s left." % money)
-            rounds += 1
+    if (r + r2) != 7:
+        money -= 1
+        print("You lost your bet")
+        print("You have  $ %s left." % money)
+        rounds += 1
 
 print("You lost all of your money")
 

9eb66f444d0d0ce95af043e5f4dfd1ba2798bc8e
EdGEgar44 <34071975+EdGEgar44@users.noreply.github.com>
2018-01-11 10:50:43 -0800

    1/11/18
    Started BlackJack

--- Edgar R - Blackjack.py
+++ Edgar R - Blackjack.py
@@ -1,5 +1,31 @@
 import random
 
-dealer_card1 = random.randint()
+player_One_BlackJack = "False"
 
-dealer_card2 = random.randint()
+dealer_BlackJack = "False"
+
+while player_One_BlackJack != "False" or dealer_BlackJack != "False":
+
+    dealer_card1 = random.randint(2, 12)
+
+    dealer_card2 = random.randint(2, 12)
+
+    player_one_card1 = random.randint(2, 12)
+
+    player_one_card2 = random.randint(2, 12)
+
+    if dealer_card2 == 12:
+        dealer_card2 = "A"
+
+    if dealer_card1 == 12:
+        dealer_card1 = "A"
+
+    if player_one_card1 == 12:
+        dealer_card1 = "A"
+
+    if player_one_card2 == 12:
+        player_one_card2 = "A"
+
+    print(player_one_card1)
+
+    print(player_one_card2)

55987e24b18ca7e514c51a4079464dca6f9e0f2d
EdGEgar44 <34071975+EdGEgar44@users.noreply.github.com>
2018-01-16 10:50:26 -0800

    1/16/18
    Done work to BlackJack.

--- Edgar R - Blackjack.py
+++ Edgar R - Blackjack.py
@@ -4,7 +4,7 @@ player_One_BlackJack = "False"
 
 dealer_BlackJack = "False"
 
-while player_One_BlackJack != "False" or dealer_BlackJack != "False":
+while player_One_BlackJack != "True" or dealer_BlackJack != "True":
 
     dealer_card1 = random.randint(2, 12)
 
@@ -26,6 +26,42 @@ while player_One_BlackJack != "False" or dealer_BlackJack != "False":
     if player_one_card2 == 12:
         player_one_card2 = "A"
 
-    print(player_one_card1)
+    print("Your card %s" % player_one_card1)
 
-    print(player_one_card2)
+    print("Your card %s" % player_one_card2)
+
+    dealer_added = dealer_card1 + dealer_card2
+
+    player_one_added = player_one_card1 + player_one_card2
+
+    print("Together, the card are equal to %s." % player_one_added)
+
+    print("Dealer right_side up is %s" % dealer_card2)
+
+    if player_one_added < 21:
+        new_card = input("Would you like to get another card?(Yes or No) ")
+
+        print(dealer_card1)
+
+        if new_card == "Yes":
+            player_one_card3 = random.randint(2, 12)
+
+            if player_one_card3 == 12:
+                player_one_card3 = "A"
+
+            player_one_added = player_one_added + player_one_card3
+
+            print("Your new card is %s" % player_one_card3)
+
+            print("Together, the cards are equal to %s." % player_one_added)
+
+        if new_card == "No":
+            print("nothing")
+
+    if player_one_added > 21 or dealer_added > 21:
+        if player_one_added > 21:
+            print("You Busted. The dealer Won.")
+            print("The dealers card were %s." % player_one_added)
+
+        if dealer_added > 21:
+            print("You won. The dealer lost.")

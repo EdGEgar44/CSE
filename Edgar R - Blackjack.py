4e4a893937c1c0a44a2bb0db48b29af2cde58228
EdGEgar44 <34071975+EdGEgar44@users.noreply.github.com>
2018-01-17 10:50:18 -0800

    1/17/18
    Done more work with BlackJack.

--- Edgar R - Blackjack.py
+++ Edgar R - Blackjack.py
@@ -4,6 +4,8 @@ player_One_BlackJack = "False"
 
 dealer_BlackJack = "False"
 
+print("Welcome to BlackJack.")
+
 while player_One_BlackJack != "True" or dealer_BlackJack != "True":
 
     dealer_card1 = random.randint(2, 12)
@@ -38,27 +40,49 @@ while player_One_BlackJack != "True" or dealer_BlackJack != "True":
 
     print("Dealer right_side up is %s" % dealer_card2)
 
-    if player_one_added < 21:
-        new_card = input("Would you like to get another card?(Yes or No) ")
+    if dealer_added > 21:
+        if dealer_card2 == "A" and dealer_card1 == "A":
+            dealer_card1 = 1
+        if dealer_card1 == "A":
+            dealer_card1 = 1
 
-        print(dealer_card1)
+    if player_one_added or dealer_added < 21:
+        new_card = input("Would you like to get another card?(Yes or No) ")
 
         if new_card == "Yes":
             player_one_card3 = random.randint(2, 12)
+
+            if dealer_added > 21:
+                dealer_card3 = random.randint(2, 12)
+
+            if dealer_card3 == 12:
+                dealer_card3 = "A"
 
             if player_one_card3 == 12:
                 player_one_card3 = "A"
 
             player_one_added = player_one_added + player_one_card3
 
+            dealer_added = dealer_added + dealer_card3
+
             print("Your new card is %s" % player_one_card3)
 
             print("Together, the cards are equal to %s." % player_one_added)
 
-        if new_card == "No":
-            print("nothing")
+            if player_one_added or dealer_added < 21:
+                new_card2 = input("Would you like get another card?(Yes or No) ")
 
-    if player_one_added > 21 or dealer_added > 21:
+                if new_card2 == "Yes":
+                    player_one_card4 = random.randint(2, 12)
+
+                if new_card2 == "No":
+                    print("You are not going to get a new card.")
+
+        if new_card == "No":
+            print("You are not going to get any more cards.")
+
+
+    if player_one_added or dealer_added > 21:
         if player_one_added > 21:
             print("You Busted. The dealer Won.")
             print("The dealers card were %s." % player_one_added)

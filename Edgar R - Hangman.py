c72545e149abed180c486859c7ad7245256ce34d
EdGEgar44 <34071975+EdGEgar44@users.noreply.github.com>
2018-01-25 10:49:22 -0800

    1/25/18
    Doing step 4.

--- Edgar R - Hangman.py
+++ Edgar R - Hangman.py
@@ -5,17 +5,10 @@ guesses_left = 10
 
 player_stop_playing = "False"
 
-punctuation = string.punctuation
-
-lowercase_letters = string.ascii_lowercase
-
-uppercase_letters = string.ascii_uppercase
-
 word_phrase_bank = ["Classwork", "Train", "Computer", "Blackjack", "NaNi", "Firefox", "Lucky", "Window",
                     "Terraria", "Mr.Wiebe"]
 
 guessed_words = []
-
 
 while player_stop_playing != "True":
 
@@ -23,9 +16,11 @@ while player_stop_playing != "True":
 
     play_a_game = play_a_game.lower()
 
-    while play_a_game == "yes" and guesses_left >= 0:
+    word_selector = random.choice(word_phrase_bank)
 
-        word_selector = random.choice(word_phrase_bank)
+    word_selector_stars_list = list(word_selector_stars)
+
+    while play_a_game == "yes" and guesses_left >= 0:
 
         range_of_letter = len(word_selector)
 
@@ -34,15 +29,20 @@ while player_stop_playing != "True":
         if word_selector == "Mr.Wiebe":
             word_selector_stars = "**.*****"
 
-        print(word_selector_stars)
-
         word_selector = word_selector.lower()
+
+        word_selector = list(word_selector)
 
         while guesses_left >= 0:
 
-            print("These are the words you have guessed %s " % guessed_words)
+            if guessed_words == str([]):
+                print("These are the words you have guessed %s " % guessed_words)
 
             print("You have %s guesses left." % guesses_left)
+
+            print(word_selector_stars)
+
+            print(word_selector)
 
             word_guessed = input("Guess the letter. ")
 
@@ -50,14 +50,36 @@ while player_stop_playing != "True":
 
             if word_guessed != word_selector:
                 word_guessed = guessed_words.append(word_guessed)
+                print("Try again")
                 guesses_left -= 1
 
             print(guessed_words)
 
-            if word_guessed == word_selector:
+            if word_selector == "classwork" and word_guessed == "c" or "l" or "a" or "s" or "w" or "o" or "r" or "k":
                 print("You got it right!")
+                if word_guessed == "c":
+                    word_selector_stars_list[0] = "c"
 
-                print("You have %s guesses left." % guesses_left)
+                if word_guessed == "l":
+                    word_selector_stars_list[1] = "l"
+
+                if word_selector_stars == "a":
+                    word_selector_stars_list[2] = "a"
+
+                if word_selector_stars == "s":
+                    word_selector_stars_list[3, 4] = "s"
+
+                if word_selector_stars == "w":
+                    word_selector_stars_list[5] = "w"
+
+                if word_selector_stars == "o":
+                    word_selector_stars_list[8] = "o"
+
+                if word_selector_stars == "r":
+                    word_selector_stars_list[7] = "r"
+
+                if word_selector_stars == "k":
+                    word_selector_stars_list[9] = "k"
 
                 if guesses_left == 10:
                     print("You did it in first Try. You must have cheated.")
